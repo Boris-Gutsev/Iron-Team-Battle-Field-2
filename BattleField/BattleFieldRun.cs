250fea6f510732e005405d7fa2b479e1095b86b4
Boris-Gutsev <github@gutsev.com>
2013-05-11 16:47:19 +0300

    export InputFielsSize fix bug input field size

--- BattleField/BattleFieldRun.cs
+++ BattleField/BattleFieldRun.cs
@@ -8,46 +8,80 @@ namespace BattleField
 {
     class BattleFieldRun
     {
+        public static readonly int MIN_FIELD_SIZE = 0;
+        public static readonly int MAX_FIELD_SIZE = 11;
+
+        public static readonly int MIN_BORDER = 0;
+        public static readonly string EMPTY_CELL = " - ";
+
         public static void Main(string[] args)
         {
-            int fieldSize;
-           string tempFieldSize;
-           Console.WriteLine("Welcome to the Battle Field game");
-           do
-           {
-              Console.Write("Enter legal size of board: ");
-              tempFieldSize = Console.ReadLine();
-           } while ((!Int32.TryParse(tempFieldSize, out fieldSize)) || (fieldSize < 0) || (fieldSize > 11));
+            int fieldSize = InputFielsSize();
 
-           BattleFieldFramefork bf = new BattleFieldFramefork(fieldSize);
-           bf.InitField();
-           bf.InitMines();
-           bf.DisplayField();
+            BattleFieldFramefork bf = new BattleFieldFramefork(fieldSize);
+            bf.InitField();
+            bf.InitMines();
+            bf.DisplayField();
 
-           string coordinates;
-           int XCoord, YCoord;
+           
+            int coordX = 0;
+            int coordY = 0;
 
-           do
-           {
-              do
-              {
-                 Console.Write("Enter coordinates: ");
-                 coordinates = Console.ReadLine();
-                 XCoord = Convert.ToInt32(coordinates.Substring(0, 1));
-                 YCoord = Convert.ToInt32(coordinates.Substring(2));
+            do
+            {
+                bool isInField = true;
+                bool isEmptyCell = false;
+                do
+                {
+                    //TODO: do try parse, correct parse
+                    Console.Write("Enter coordinates: ");
+                    string coordinates = Console.ReadLine();
+                    coordX = Convert.ToInt32(coordinates.Substring(0, 1));
+                    coordY = Convert.ToInt32(coordinates.Substring(2));
 
-                 if ((XCoord < 0) || (YCoord > fieldSize - 1) || (bf.Pozicii[XCoord, YCoord] == " - "))
-                    Console.WriteLine("Invalid Move");
-              } while ((XCoord < 0) || (YCoord > fieldSize - 1) || (bf.Pozicii[XCoord, YCoord] == " - "));
+                    isInField = (coordX >= MIN_BORDER) && (coordX <= fieldSize - 1) && 
+                        (coordY >= MIN_BORDER) && (coordY <= fieldSize - 1);
+                    if (isInField)
+                    {
+                        isEmptyCell = (bf.Pozicii[coordX, coordY] == EMPTY_CELL);
+                    }
+                   
+                    if (!isInField || isEmptyCell)
+                    {
+                        Console.WriteLine("Invalid Move");
+                    }
+                }
+                while (!isInField || isEmptyCell);
 
-              bf.DetonateMine(XCoord, YCoord);
-              bf.DisplayField();
-              bf.DetonatedMines++;
-           } while (bf.PrebroiOstavashtiteMinichki() != 0);
+                bf.DetonateMine(coordX, coordY);
+                bf.DisplayField();
+                bf.DetonatedMines++;
+            }
+            while (bf.PrebroiOstavashtiteMinichki() != 0);
 
-           Console.WriteLine("Game Over. Detonated Mines: " + bf.DetonatedMines);
-           Console.ReadKey();
+            Console.WriteLine("Game Over. Detonated Mines: " + bf.DetonatedMines);
+            Console.ReadKey();
         }
-
+  
+        private static int InputFielsSize()
+        {
+            int fieldSize;
+            string fieldSizeStr;
+            bool isInputSizeParseCorrect = false;
+            bool isFieldSizeCorrect = false;
+            Console.WriteLine("Welcome to the Battle Field game");
+            do
+            {
+                Console.Write("Enter legal size of board: ");
+                fieldSizeStr = Console.ReadLine();
+                isInputSizeParseCorrect = Int32.TryParse(fieldSizeStr, out fieldSize);
+                if (isInputSizeParseCorrect)
+                {
+                    isFieldSizeCorrect = (fieldSize > MIN_FIELD_SIZE) && (fieldSize < MAX_FIELD_SIZE);
+                }
+            }
+            while (!isFieldSizeCorrect);
+            return fieldSize;
+        }
     }
 }
